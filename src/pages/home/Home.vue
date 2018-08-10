372f377d1409966391cfc28d5fa2e7cc97389d83
滕子健 <tengzijian@turingsec.com>
2018-08-10 16:13:19 +0800

    2018年08月10日16:13:18

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>home</div>
+</template>
+
+<script>
+// 导出一个对象
+export default {
+  // 组件的名字
+  name: 'Home'
+}
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// @代表src目录下
+import Home from '@/pages/home/Home'
+import List from '@/pages/list/List'
 
 Vue.use(Router)
 
@@ -8,8 +10,15 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home', // 路由项的名字
+      // 当用户访问根路径的时候展示Home这个组件
+      // <router-view/> 所显示的内容 就是这里配置的组件
+      component: Home
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List
     }
   ]
 })
