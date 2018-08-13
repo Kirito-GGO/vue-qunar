498015d3d6c301d6006eb18fe186b424bb51e54e
滕子健 <tengzijian@turingsec.com>
2018-08-13 18:47:00 +0800

    7-7 推荐 周末游组件开发

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom"
+          v-for="item of recommendList"
+          :key="item.id">
+        <img class="item-img"
+             :src="item.imgUrl"
+             alt="">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg',
+        title: '故宫',
+        desc: '金銮殿名为太和殿，是皇帝登基和举行大典的地方'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg',
+        title: '故宫',
+        desc: '金銮殿名为太和殿，是皇帝登基和举行大典的地方'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg',
+        title: '故宫',
+        desc: '金銮殿名为太和殿，是皇帝登基和举行大典的地方'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+  margin-top 0.2rem
+  line-height 0.8rem
+  background #eee
+  /* text-indent 属性规定文本块中首行文本的缩进 */
+  text-indent 0.2rem
+.item
+  overflow hidden
+  display flex
+  height 1.9rem
+  .item-img
+    width 1.7rem
+    height 1.7rem
+    padding 0.1rem
+  .item-info
+    flex 1
+    padding 0.1rem
+    min-width 0
+    .item-title
+      line-height 0.54rem
+      font-size 0.32rem
+      ellipsis()
+    .item-desc
+      line-height 0.4rem
+      color #ccc
+      ellipsis()
+    .item-button
+      line-height 0.44rem
+      margin-top 0.16rem
+      background #ff9300
+      padding 0 0.2rem
+      border-radius 0.06rem
+      color #fff
+</style>
---
+++ src/pages/home/components/weekend.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="title">周末去哪</div>
+    <ul>
+      <li class="item border-bottom"
+          v-for="item of recommendList"
+          :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img"
+               :src="item.imgUrl"
+               alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/53/02091f1ee43d0a.jpg_r_640x214_2e3256b1.jpg',
+        title: '北京清凉避暑胜地',
+        desc: '到大自然中享受“天然空调”的清凉'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/53/02091f1ee43d0a.jpg_r_640x214_2e3256b1.jpg',
+        title: '北京清凉避暑胜地',
+        desc: '到大自然中享受“天然空调”的清凉'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/53/02091f1ee43d0a.jpg_r_640x214_2e3256b1.jpg',
+        title: '北京清凉避暑胜地',
+        desc: '到大自然中享受“天然空调”的清凉'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+  margin-top 0.2rem
+  line-height 0.8rem
+  background #eee
+  /* text-indent 属性规定文本块中首行文本的缩进 */
+  text-indent 0.2rem
+.item-img-wrapper
+  height 0
+  overflow hidden
+  padding-bottom 33.9%
+  .item-img
+    width 100%
+.item-info
+  padding 0.1rem
+  .item-title
+    line-height 0.54rem
+    font-size 0.32rem
+    ellipsis()
+  .item-desc
+    line-height 0.4rem
+    color #ccc
+    ellipsis()
+</style>
