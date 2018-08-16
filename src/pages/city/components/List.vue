4ff844e28e53b927ac02f69fc82ee1dba5402c2f
滕子健 <tengzijian@turingsec.com>
2018-08-16 23:44:28 +0800

    8-4 better-scroll插件的使用

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="list"
+       ref="wrapper">
+    <!-- ref="wrapper" 获取dom -->
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+// better-scroll 是一款重点解决移动端各种滚动场景需求的插件
+import Bscroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  // 生命周期函数 在dom挂载完毕执行
+  mounted () {
+    // 创建better-scroll的实例 实例创建的时候要接收一个dom元素或者dom选择器
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.border-topbottom
+  &:before
+    border-color #ccc
+  &:after
+    border-color #ccc
+.border-bottom
+  &:before
+    border-color #ccc
+.list
+  overflow hidden
+  position absolute
+  top 1.58rem
+  left 0
+  right 0
+  bottom 0
+  .title
+    line-height 0.54rem
+    background #eee
+    padding-left 0.2rem
+    color #666
+    font-size 0.26rem
+  .button-list
+    overflow hidden
+    padding 0.1rem 0.6rem 0.1rem 0.1rem
+    .button-wrapper
+      width 33.33%
+      float left
+      .button
+        text-align center
+        margin 0.1rem
+        padding 0.1rem 0
+        border 0.02rem solid #ccc
+        border-redius 0.06rem
+  .item-list
+    .item
+      line-height 0.76rem
+      padding-left 0.2rem
+</style>
